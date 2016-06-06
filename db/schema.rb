e48d0b09c33d7fcfd119d58b4c1fd67390af8340
Lamine Ndaw <brahimndaw@gmail.com>
2016-06-06 12:48:12 -0400

    Post background

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160606144832) do
+ActiveRecord::Schema.define(version: 20160606153451) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "ratings", force: :cascade do |t|
+  end
+
+  create_table "recipe_amounts", force: :cascade do |t|
+    t.integer "recipes_id"
+    t.integer "ingredients_id"
   end
 
   create_table "recipes", force: :cascade do |t|
