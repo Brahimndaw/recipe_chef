e464e65243f42f8003b963a6fc192430a098b4fc
Lamine Ndaw <brahimndaw@gmail.com>
2016-06-06 11:51:18 -0400

    pushing to heroku

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -4,7 +4,7 @@ class RecipesController < ApplicationController
 
   def search
     # Adapter::FoodAPIWrapper.new(params[:query])
-    @recipes = Recipe.where("lower(title) LIKE ? OR tags LIKE ?", "%#{params[:q].downcase}%")
+    @recipes = Recipe.where("lower(title) LIKE ?", "%#{params[:q].downcase}%")
     if @recipes.nil?
       flash[:danger] = "Sorry, no results"
       redirect_to recipes_path
