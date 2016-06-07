1ef759b92e3c62c686fbe0595b6b6f796eb9b7c5
dcarrano <dcarrano@gmail.com>
2016-06-07 11:49:56 -0400

    reject duplicate user test

---
+++ spec/controllers/users_controller_spec.rb
@@ -0,0 +1,17 @@
+# require 'rails_helper'
+#
+# RSpec.describe UsersController do
+#   describe "GET index" do
+#     it "assigns @users" do
+#       mario = User.create(username: "Mario", name: "Mario Mario", email: "mario@mario.com")
+#       get :index
+#       binding.pry
+#       expect(assigns(:users)).to eq([user])
+#     end
+#
+#     it "renders the index template" do
+#       get :index
+#       expect(response).to render_template("index")
+#     end
+#   end
+# end
